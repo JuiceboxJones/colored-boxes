fe96a2820e5bf9b1101f02c5ec6534357dcf9ec2
Juicebox Jones <jderhalli@gmail.com>
2019-03-02 15:23:27 -0800

    Updates

---
+++ precache-manifest.c09e6a46e08440b964b84012ee6e753e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa9bd37747ab87a167af",
+    "url": "/colored-boxes/static/css/main.1af95d6f.chunk.css"
+  },
+  {
+    "revision": "aa9bd37747ab87a167af",
+    "url": "/colored-boxes/static/js/main.aa9bd377.chunk.js"
+  },
+  {
+    "revision": "767b2f6dd943fbd904e5",
+    "url": "/colored-boxes/static/js/runtime~main.767b2f6d.js"
+  },
+  {
+    "revision": "015ea03cb7b11a05a1c5",
+    "url": "/colored-boxes/static/js/2.015ea03c.chunk.js"
+  },
+  {
+    "revision": "8abff18f7dd5faeca351ff42d29e91c2",
+    "url": "/colored-boxes/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/colored-boxes/precache-manifest.c09e6a46e08440b964b84012ee6e753e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/colored-boxes/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
